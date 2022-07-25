f1d8bbca325f5b31eda99db0ba5e869834a64f5a
Mac-Barrett <maccabee.barrett3@gmail.com>
2022-07-25 16:15:31 -0500

    Changed pokedex page & pokerow to use a table

--- src/lib/_components/Pokerow.svelte
+++ src/lib/_components/Pokerow.svelte
@@ -4,45 +4,38 @@
     export var data : IPokemon;
 </script>
 
-<div class="row">
-    <div class="col">
-        <p>{data.DexNum}</p>
-    </div>
-    <div class="col">
-        <p><a href={`./pokemon/${data.DexNum}`}>{data.Name}</a></p>
-    </div>
-    <div class="col">
+<tr>
+    <td>
+        <p class="d-md-block d-none">{data.DexNum}</p>
+        <small class="d-md-none d-block">{data.DexNum}</small>
+    </td>
+    <td>
+        <p class="d-md-block d-none"><a href={`./pokemon/${data.DexNum}`}>{data.Name}</a></p>
+        <small class="d-md-none d-block"><a href={`./pokemon/${data.DexNum}`}>{data.Name}</a></small>
+    </td>
+    <td>
         <img src={data.Sprite} alt={data.Name}>
-    </div>
-    <div class="col">
+    </td>
+    <td>
         {#if data.Types.split("|").length > 1}
-        <img class="type-icon" src={`/res/${data.Types.split("|")[0]}.png`} alt={data.Types.split("|")[0]}>
-        <img class="type-icon" src={`/res/${data.Types.split("|")[1]}.png`} alt={data.Types.split("|")[1]}>
+            <img class="type-icon" src={`/res/${data.Types.split("|")[0]}.png`} alt={data.Types.split("|")[0]}>
+            <img class="type-icon" src={`/res/${data.Types.split("|")[1]}.png`} alt={data.Types.split("|")[1]}>
         {:else}
-        <img class="type-icon" src={`/res/${data.Types}.png`} alt={data.Types}>
+            <img class="type-icon" src={`/res/${data.Types}.png`} alt={data.Types}>
         {/if}
-    </div>
+    </td>
     {#each data.Stats as stat, i}
-    <div class={`col stat-${i}`}>
-        <p>{stat}</p>
-    </div>
+        <td class={`data stat-${i}`}>
+            <p class="d-md-block d-none">{stat}</p>
+            <small class="d-md-none d-block">{stat}</small>
+        </td>
     {/each}
-</div>
+</tr>
 
 <style>
-    .row {
-        max-height: 64px;
-    }
-    .col {
-        border: 1px solid black;
-        width: 10%;
-        padding:0;
-    }
-    p {
+    td {
         text-align: center;
-        font-family: monospace;
-        padding:0;
-        margin:0;
+        border: 1px solid black;
     }
     a {
         color: black;
--- src/routes/pokedex.svelte
+++ src/routes/pokedex.svelte
@@ -1,39 +1,41 @@
 <script lang="ts">
-import GridList, { ListType } from "$lib/_components/GridList.svelte";
-
-
+    import GridList, { ListType } from "$lib/_components/GridList.svelte";
 </script>
 
-<div class="container my-5">
-    <div class="row sticky-top">
-        <div id="Dex" class="col">Dex No</div>
-        <div id="Name" class="col">Name</div>
-        <div id="Sprite" class="col">Sprite</div>
-        <div id="Types" class="col">Types</div>
-        <div id="HP" class="col">HP</div>
-        <div id="Atk" class="col">Atk</div>
-        <div id="Def" class="col">Def</div>
-        <div id="SpAtk" class="col">SpAtk</div>
-        <div id="SpDef" class="col">SpDef</div>
-        <div id="Speed" class="col">Speed</div>
-    </div>
-    <GridList listType={ListType.PKMN_LIST} IDs={[...Array(151).keys()]}/>
+<div class="container my-5 p-5">
+    <table>
+        <tr class="sticky-top">
+            <th id="Dex">Dex No</th>
+            <th id="Name">Name</th>
+            <th id="Sprite">Sprite</th>
+            <th id="Types">Types</th>
+            <th id="HP">HP</th>
+            <th id="Atk">Atk</th>
+            <th id="Def">Def</th>
+            <th id="SpAtk">SpAtk</th>
+            <th id="SpDef">SpDef</th>
+            <th id="Speed">Speed</th>
+        </tr>
+        <GridList listType={ListType.PKMN_LIST} IDs={[...Array(151).keys()]}/>
+    </table>
 </div>
 
 <style>
-    .row {
-        max-height: 64px;
+    .container {
+        background-color: darkslateblue;
+        border-radius: 20px;
+        border: 2px solid black;
     }
-    .col {
-        border: 1px solid black;
-        width:10%;
+    table {
+        background-color: white;
+        width: 100%;
+    }
+    th {
         text-align: center;
-
-        font-family: monospace;
-        font-size: 1.25em;
-        background-color: slategray;
+        background-color: white;
+        border: 1px solid black;
     }
-
+    
     #HP {
         background-color: red;
     }
